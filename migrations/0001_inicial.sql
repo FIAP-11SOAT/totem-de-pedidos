a6ba4017f76206150c8a80757c50c0a1ddba4cbc
Matheus Ribeiro <teteuoliveira12@gmail.com>
2025-04-22 22:27:46 -0300

    feat(migrations): criar tabelas iniciais para o sistema de pedidos
    - Criada a tabela 'costumers' para armazenar informações dos clientes.
    - Criada a tabela 'orders' para registrar os pedidos realizados.
    - Criada a tabela 'categories_products' para categorizar os produtos.
    - Criada a tabela 'products' para armazenar detalhes dos produtos disponíveis.
    - Criada a tabela 'order_items' para relacionar produtos aos pedidos.
    
    Essas alterações estabelecem a estrutura de banco de dados necessária para o funcionamento do sistema de pedidos, permitindo a gestão eficiente de clientes, pedidos e produtos. A implementação dessas tabelas facilita a expansão futura do sistema e melhora a organização dos dados.

---
+++ migrations/0001_inicial.sql
@@ -0,0 +1,46 @@
+create table if not exists costumers (
+    id serial primary key,
+    name varchar(100) not null,
+    email varchar(100) not null unique,
+    tax_id varchar(50) not null,
+    created_at timestamp default current_timestamp
+);
+
+create table if not exists orders (
+    id serial primary key,
+    order_date timestamp default current_timestamp,
+    status varchar(50) not null,
+    total_amount numeric(10, 2) not null,
+    created_at timestamp default current_timestamp
+    
+    customer_id integer references costumers(id) on delete cascade,
+);
+
+create table if not exists categories_products (
+    id serial primary key,
+    name varchar(100) not null,
+    description text,
+    created_at timestamp default current_timestamp
+);
+
+create table if not exists products (
+    id serial primary key,
+    name varchar(100) not null,
+    description text,
+    price numeric(10, 2) not null,
+    image_url varchar(255),
+    preparation_time integer not null,
+    created_at timestamp default current_timestamp
+
+    category_id integer references categories_products(id) on delete cascade,
+);
+
+create table if not exists order_items (
+    id serial primary key, 
+    quantity integer not null,
+    price numeric(10, 2) not null,
+    created_at timestamp default current_timestamp 
+
+    order_id integer references orders(id) on delete cascade,
+    product_id integer references products(id) on delete cascade
+);
