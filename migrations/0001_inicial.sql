7ee7f2756087c18c5ca17f9150d90642f176bf74
Lucas <lucasdearaujo.brandao@gmail.com>
2025-04-29 21:19:08 -0300

    chore: database scheme

--- migrations/0001_inicial.sql
+++ migrations/0001_inicial.sql
@@ -3,7 +3,8 @@ create table if not exists costumers (
     name varchar(100) not null,
     email varchar(100) not null unique,
     tax_id varchar(50) not null,
-    created_at timestamp default current_timestamp
+    created_at timestamp default current_timestamp,
+    updated_at timestamp default current_timestamp
 );
 
 create table if not exists orders (
@@ -12,15 +13,17 @@ create table if not exists orders (
     status varchar(50) not null,
     total_amount numeric(10, 2) not null,
     created_at timestamp default current_timestamp,
+    updated_at timestamp default current_timestamp,
     
     customer_id integer references costumers(id) on delete cascade
 );
 
-create table if not exists categories_products (
+create table if not exists product_categories (
     id serial primary key,
     name varchar(100) not null,
     description text,
-    created_at timestamp default current_timestamp
+    created_at timestamp default current_timestamp,
+    updated_at timestamp default current_timestamp
 );
 
 create table if not exists products (
@@ -31,8 +34,9 @@ create table if not exists products (
     image_url varchar(255),
     preparation_time integer not null,
     created_at timestamp default current_timestamp,
+    updated_at timestamp default current_timestamp,
 
-    category_id integer references categories_products(id) on delete cascade
+    category_id integer references product_categories(id) on delete cascade
 );
 
 create table if not exists order_items (
@@ -40,6 +44,7 @@ create table if not exists order_items (
     quantity integer not null,
     price numeric(10, 2) not null,
     created_at timestamp default current_timestamp,
+    updated_at timestamp default current_timestamp,
 
     order_id integer references orders(id) on delete cascade,
     product_id integer references products(id) on delete cascade
