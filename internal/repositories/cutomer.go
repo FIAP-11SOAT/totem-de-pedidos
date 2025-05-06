612ad5914a79eb87f51346c233eae7635a070295
Vinícius Branco <branco.vinicius@ifood.com.br>
2025-05-06 20:02:10 -0300

    feat: repository create customer

--- internal/repositories/cutomer.go
+++ internal/repositories/cutomer.go
@@ -1,6 +1,9 @@
 package repositories
 
 import (
+	"context"
+	"fmt"
+
 	dbadapter "github.com/FIAP-11SOAT/totem-de-pedidos/internal/adapters/database"
 	"github.com/FIAP-11SOAT/totem-de-pedidos/internal/core/domain/entity"
 	"github.com/FIAP-11SOAT/totem-de-pedidos/internal/core/ports/repositories"
@@ -17,6 +20,19 @@ func NewCustomerRepository(database *dbadapter.DatabaseAdapter) repositories.Cus
 	}
 }
 
-func (s *CustomerRepository) CreateCustomer(Customer *entity.Customer) (*entity.Customer, error) {
-	return nil, nil
+func (s *CustomerRepository) CreateCustomer(customer *entity.Customer) (*entity.Customer, error) {
+	query := `INSERT INTO customers (id, name, email, tax_id) VALUES (@id, @name, @email, @taxid)`
+	args := pgx.NamedArgs{
+		"id":    customer.ID,
+		"name":  customer.Name,
+		"email": customer.Email,
+		"taxid": customer.TaxID,
+	}
+
+	_, err := s.sqlClient.Exec(context.Background(), query, args)
+	if err != nil {
+		return nil, fmt.Errorf("error on inserting Customer: %w", err)
+	}
+
+	return customer, nil
 }
