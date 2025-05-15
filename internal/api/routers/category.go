9be7dd54b110ea0a9f36ba3d652326219a970391
FelipeBiancardi <felipevp218@gmail.com>
2025-05-14 23:30:18 -0300

    create category

--- internal/api/routers/category.go
+++ internal/api/routers/category.go
@@ -2,11 +2,16 @@ package routers
 
 import (
 	dbadapter "github.com/FIAP-11SOAT/totem-de-pedidos/internal/adapters/database"
+	"github.com/FIAP-11SOAT/totem-de-pedidos/internal/api/handlers"
 	"github.com/labstack/echo/v4"
 )
 
 func CategoryRouter(e *echo.Echo, dbConnection *dbadapter.DatabaseAdapter) {
-	e.GET("/categories", func(c echo.Context) error {
-		return c.JSON(200, "ok")
-	})
+	h := handlers.NewCategoryHandler(dbConnection)
+
+	e.GET("/categories", h.ListAllCategories)
+	e.GET("/categories/:id", h.FindCategoryById)
+	e.POST("/categories", h.CreateCategory)
+	e.PUT("/categories/:id", h.UpdateCategory)
+	e.DELETE("/categories/:id", h.DeleteCategory)
 }
--- internal/core/ports/usecase/category.go
+++ internal/core/ports/usecase/category.go
@@ -1,1 +1,18 @@
 package usecase
+
+import (
+	"github.com/FIAP-11SOAT/totem-de-pedidos/internal/core/domain/entity"
+	"github.com/FIAP-11SOAT/totem-de-pedidos/internal/core/ports/inputs"
+)
+
+type Category interface {
+	GetCategories() ([]*entity.Category, error)
+
+	CreateCategory(categoryDTO *inputs.CategoryInput) (*entity.Category, error)
+
+	UpdateCategory(categoryDTO *entity.Category) (*entity.Category, error)
+
+	DeleteCategory(categoryID int) error
+
+	FindCategoryById(categoryID int) (*entity.Category, error)
+}
